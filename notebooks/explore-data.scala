e597fe3bbb2cc3aed6acd1c9e136047089ee2799
luis-israel-velez <40527357+luis-israel-velez@users.noreply.github.com>
2019-10-03 13:11:35 -0400

    v 1.0.1

--- notebooks/explore-data.scala
+++ notebooks/explore-data.scala
@@ -7,8 +7,3 @@ val storeDF = spark.read.format("delta").load(path+"curated/storeTransactions")
 
 
 display(storeDF.filter($"country" === "US").groupBy("brand").agg(count("*").alias("transactions")).orderBy("brand"))
-
-
-// COMMAND ----------
-
-display(dbutils.fs.ls(path+"landing/lv-iot-streaming/01/" + year + month  + day))
