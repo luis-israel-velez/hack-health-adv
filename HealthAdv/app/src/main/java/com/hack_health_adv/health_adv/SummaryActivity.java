e2ca9514ad05b028628140a565c8edfde7101ddc
Luis Velez <luis_training@outlook.com>
2019-10-03 21:26:44 -0400

    v 1.0.1

---
+++ HealthAdv/app/src/main/java/com/hack_health_adv/health_adv/SummaryActivity.java
@@ -0,0 +1,14 @@
+package com.hack_health_adv.health_adv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
