0b83d98f516a373dfcca087ae52ae2c88b3cfbe1
luis-israel-velez <40527357+luis-israel-velez@users.noreply.github.com>
2019-10-03 23:47:38 -0400

    v 1.2

--- notebooks/database.scala
+++ notebooks/database.scala
@@ -24,3 +24,74 @@ collection.show()
 // COMMAND ----------
 
 display(collection.filter($"username" === "abhinav@gmail.com"))
+
+// COMMAND ----------
+
+Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+
+// COMMAND ----------
+
+val jdbcHostname = "luishackdbserver.database.windows.net"
+val jdbcPort = 1433
+val jdbcDatabase = "hackdb"
+val jdbcUsername = "hackadmin"
+val jdbcPassword = "Hack@2019"
+
+// Create the JDBC URL without passing in the user and password parameters.
+val jdbcUrl = s"jdbc:sqlserver://${jdbcHostname}:${jdbcPort};database=${jdbcDatabase}"
+
+// Create a Properties() object to hold the parameters.
+import java.util.Properties
+val connectionProperties = new Properties()
+
+connectionProperties.put("user", s"${jdbcUsername}")
+connectionProperties.put("password", s"${jdbcPassword}")
+
+// COMMAND ----------
+
+val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+connectionProperties.setProperty("Driver", driverClass)
+
+// COMMAND ----------
+
+val employees_table = spark.read.jdbc(jdbcUrl, "user_t", connectionProperties)
+employees_table.createOrReplaceTempView("user_t")
+
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC 
+// MAGIC select * from user_t;
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC 
+// MAGIC drop table user_t; 
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC CREATE TABLE user_t
+// MAGIC   (username String, password String, household_count INT, created_date timestamp, last_login timestamp)
+// MAGIC   USING PARQUET --PARTITIONED BY (username)
+// MAGIC   OPTIONS ('compression'='snappy');
+
+// COMMAND ----------
+
+// MAGIC %sql  
+// MAGIC INSERT OVERWRITE table user_t 
+// MAGIC   select "r1" as username, "r12" as password, 10 as household_count, null , null;
+
+// COMMAND ----------
+
+// MAGIC %sql  
+// MAGIC select * from user_t;
+
+// COMMAND ----------
+
+spark.table("user_t")
+     .write
+     .mode(SaveMode.Append) // <--- Append to the existing table
+     .jdbc(jdbcUrl, "user_t", connectionProperties)
