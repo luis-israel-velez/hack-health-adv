008e33cc6f27ce98ee4849bf78d7080dbad8d9d7
Luis Velez <luis_training@outlook.com>
2019-10-03 22:16:11 -0400

    v 1.0.1.2

--- HealthAdv/app/src/main/java/com/hack_health_adv/health_adv/LoginActivity.java
+++ HealthAdv/app/src/main/java/com/hack_health_adv/health_adv/LoginActivity.java
@@ -87,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    Toast.makeText(LoginActivity.this,"Login Error",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this,"Login Error!",Toast.LENGTH_SHORT).show();
                 }
             }
         });
